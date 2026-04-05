b833bfa0192d899cc32cfdeabb4d9eebc48fe59f
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-04-05 12:07:01 +0530

    improved projects section

--- script.js
+++ script.js
@@ -49,3 +49,22 @@ navLinks.forEach(link => {
         navbar.classList.remove('open');
     });
 });
+
+/* Swiper JS */
+new Swiper('.projects-swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 24,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        768:  { slidesPerView: 2 },
+        1100: { slidesPerView: 3 },
+    },
+});
